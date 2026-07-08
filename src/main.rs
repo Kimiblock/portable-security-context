ad8ff3a32b4560d786aa78d28ef9e53270c92a62
Kimiblock Moe <kimiblock@icloud.com>
2026-07-08 15:26:23 +0800

    main: get config

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ mod check;
 
 #[tokio::main]
 async fn main() -> std::process::ExitCode {
-
+	let conf_spawn = tokio::spawn(async {
+		info::SandboxInfo::get()
+	});
 
 	std::process::ExitCode::SUCCESS
 }
