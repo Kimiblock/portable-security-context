f9ecf5b2822e78c7044dac8d98bb347cf6047d4e
Kimiblock Moe <kimiblock@icloud.com>
2026-07-08 14:37:58 +0800

    connect: set blocking round trip mode

--- src/connect.rs
+++ src/connect.rs
@@ -4,10 +4,15 @@ use thiserror::Error;
 pub enum ConnectError{
 	#[error("Error connecting to Wayland socket: {0:#?}")]
 	ConnectError(wayrs_client::ConnectError),
+
+	#[error("Error setting blocking roundtrip mode for connection: {0:#?}")]
+	SetBlockingRoundtripErr(std::io::Error),
 }
 
 pub async fn connect() -> Result<wayrs_client::Connection<()>, ConnectError> {
-	wayrs_client::
+	let mut conn = wayrs_client::
 		Connection::connect().
-		map_err(ConnectError::ConnectError)
+		map_err(ConnectError::ConnectError)?;
+	conn.blocking_roundtrip().map_err(ConnectError::SetBlockingRoundtripErr)?;
+	Ok(conn)
 }
