932ca80cefcc99973827543fa2a185cbf9f280be
Kimiblock Moe <kimiblock@icloud.com>
2026-07-08 15:47:34 +0800

    main: check if compositor supports security context

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,48 @@ mod create_context;
 mod info;
 mod connect;
 mod check;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum MainError {
+	#[error("Could not spawn tokio thread: {0:#?}")]
+	SpawnError(tokio::task::JoinError),
+
+	#[error("Could not connect: {0:#?}")]
+	ConnectError(connect::ConnectError),
+
+	#[error("Compositor does not support security-context-v1")]
+	SecurityContextNotSupported,
+}
 
 #[tokio::main]
-async fn main() -> std::process::ExitCode {
+async fn main() -> Result<(), MainError> {
+	let conn_spawn = tokio::spawn(connect::connect());
 	let conf_spawn = tokio::spawn(async {
 		info::SandboxInfo::get()
 	});
 
-	std::process::ExitCode::SUCCESS
+	let conn = conn_spawn
+		.await
+		.map_err(MainError::SpawnError)?;
+	let conn = conn.map_err(MainError::ConnectError)?;
+
+
+	let support_spawn = tokio::spawn(check::security_context_supported(conn));
+
+	{
+		let is_supported = support_spawn.await.map_err(MainError::SpawnError);
+		match is_supported.unwrap() {
+			true	=> {}
+			false	=> {
+				return Err(
+					MainError::SecurityContextNotSupported
+				)
+			}
+		};
+	}
+
+	Ok(())
 }
 
 
